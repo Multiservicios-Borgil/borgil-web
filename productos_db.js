1eb0cbe36b841e881f93fe7237e368ff8632414d
Raul Gil <raulgilcembrano@hotmail.es>
2026-04-08 20:33:05 +0200

    Add files via upload

--- productos_db.js
+++ productos_db.js
@@ -1,1 +1,26 @@
-window.PRODUCTOS_BORGIL = [];
+window.PRODUCTOS_BORGIL = [
+    {
+        "id": 1775585318022,
+        "familia": "Frio",
+        "estado": "Nuevo con taras estéticas",
+        "titulo": "Frigorifico Corbero CC200V24NFX",
+        "descripcion": "Golpe en la puerta superior y lateral 201 x 59.5 x 60 cm No Frost",
+        "precio_original": "685€",
+        "precio_oferta": "599€",
+        "imagen": "https://i.ibb.co/275tCH80/FRI-55ee-4-removebg-preview.jpg",
+        "imagen2": "https://i.ibb.co/jvSztNpG/FRI-55ee-5-removebg-preview.jpg",
+        "imagen3": "https://i.ibb.co/d4MNxQkD/FRI-55ee-2.jpg"
+    },
+    {
+        "id": 1775585316986,
+        "familia": "Frio",
+        "estado": "Nuevo con taras estéticas",
+        "titulo": "Bolero Coolmarket combi 322 white e (02428)",
+        "descripcion": "Golpeado frontal en la parte de abajo \nrayado laterales \n\n185 cm de alto, 59,5 cm de ancho y 63,5 Fondo",
+        "precio_original": "409",
+        "precio_oferta": "349",
+        "imagen": "https://i.ibb.co/G4VcXMf1/FRI-981b-3-removebg-preview.jpg",
+        "imagen2": "https://i.ibb.co/JLZBVYt/20260323-192344-removebg-preview.jpg",
+        "imagen3": "https://i.ibb.co/0yvPMRNY/FRI-981b-4-removebg-preview.jpg"
+    }
+];
